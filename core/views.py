1b0bb80a70b8330136d26177540abf4cc3b518df
Laura Mora <lmoraballestar@gmail.com>
2018-09-23 23:28:15 +0000

    FIX: url i import

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import subprocess
 
 # Create your views here.
+def topics(request):
+	p = subprocess.Popen(["sh /confluent-4.1.1/bin/kafka-topics", "--list", "--zookeeper", "zk02-discovery:2181"], stdout=subprocess.PIPE)
+	value = str(p.communicate())
+	p.kill()
+	return HttpResponse(value)
+
