047be3831993142c86e241502c8d0cb6d6c08471
Laura Mora <lmoraballestar@gmail.com>
2018-09-23 23:58:09 +0000

    ADD: showing topics

--- core/views.py
+++ core/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import subprocess
+import os
 
 # Create your views here.
 def topics(request):
-	p = subprocess.Popen(["sh /confluent-4.1.1/bin/kafka-topics", "--list", "--zookeeper", "zk02-discovery:2181"], stdout=subprocess.PIPE)
-	value = str(p.communicate())
-	p.kill()
-	return HttpResponse(value)
+	result = os.popen('sh /confluent-4.1.1/bin/kafka-topics --list --zookeeper zk02-discovery:2181').read()
+#	os.system("sh /confluent-4.1.1/bin/kafka-topics --list --zookeeper zk02-discovery:2181")
+	#p = subprocess.Popen(["sh scripts/kafka-topics", "--list", "--zookeeper", "zk02-discovery:2181"], stdout=subprocess.PIPE)
+	#value = str(p.communicate())
+	#p.kill()
+	return HttpResponse(result)
 
