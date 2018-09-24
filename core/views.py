90cccd310101bb9a8fda30de99fb2acda3567639
Laura Mora <lmoraballestar@gmail.com>
2018-09-24 13:51:04 +0000

    FIX: remove comments

--- core/views.py
+++ core/views.py
@@ -7,9 +7,5 @@ import os
 # Create your views here.
 def topics(request):
 	result = os.popen('sh /confluent-4.1.1/bin/kafka-topics --list --zookeeper zk02-discovery:2181').read()
-#	os.system("sh /confluent-4.1.1/bin/kafka-topics --list --zookeeper zk02-discovery:2181")
-	#p = subprocess.Popen(["sh scripts/kafka-topics", "--list", "--zookeeper", "zk02-discovery:2181"], stdout=subprocess.PIPE)
-	#value = str(p.communicate())
-	#p.kill()
 	return HttpResponse(result)
 
